13833c9cef2995edf96373e1fc0cf33d78bdf1d1
Henrique Alencar <henriquemalencar@yahoo.com.br>
2026-04-10 19:48:43 -0300

    feat: dijkstra

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import csv
 import ast
+import heapq
 
 class GrafoMetro:
     def __init__(self):
@@ -54,4 +55,65 @@ class GrafoMetro:
                         no_origem = f"{estacao_atual}_{linha_comum}"
                         no_destino = f"{vizinho}_{linha_comum}"
                         self.adicionar_aresta(no_origem, no_destino, peso_viagem)
-                        
+
+def dijkstra(grafo, origem_id, destino_id):
+    nos_origem = [no for no in grafo.arestas if no.startswith(origem_id + "_")]
+    nos_destino = [no for no in grafo.arestas if no.startswith(destino_id + "_")]
+
+    if not nos_origem or not nos_destino:
+        return {"erro": "Estação não encontrada"}
+    
+    distancias = {no: float('inf') for no in grafo.arestas}
+    anteriores = {no: None for no in grafo.arestas}
+    pq = []
+
+    for no in nos_origem:
+        distancias[no] = 0
+        heapq.heappush(pq, (0, no))
+
+    no_final_encontrado = None
+
+    while pq:
+        custo_atual, no_atual = heapq.heappop(pq)
+
+        if no_atual in nos_destino:
+            no_final_encontrado = no_atual
+            break
+
+        if custo_atual > distancias[no_atual]:
+            continue
+
+        for vizinho, peso_aresta in grafo.arestas[no_atual].items():
+            novo_custo = custo_atual + peso_aresta
+
+            if novo_custo < distancias[vizinho]:
+                distancias[vizinho] = novo_custo
+                anteriores[vizinho] = no_atual
+                heapq.heappush(pq, (novo_custo, vizinho))
+
+    if no_final_encontrado is None:
+        return {"erro": "Nenhum caminho encontrado."}
+    
+    caminho_reverso = []
+    atual = no_final_encontrado
+
+    while atual is not None:
+        caminho_reverso.append(atual)
+        atual = anteriores[atual]
+
+    caminho = caminho_reverso[::-1]
+    qtd_baldeacoes = 0
+    for i in range(len(caminho) - 1):
+        estacao1 = caminho[i].split('_')[0]
+        estacao2 = caminho[i+1].split('_')[0]
+        if estacao1 == estacao2:
+            qtd_baldeacoes += 1
+
+    caminho_detalhado = [grafo.nos_dados[no] for no in caminho]
+
+    return {
+        "caminho": caminho,
+        "caminho_detalhado": caminho_detalhado,
+        "tempo_total": distancias[no_final_encontrado],
+        "qtd_baldeacoes": qtd_baldeacoes
+    }
